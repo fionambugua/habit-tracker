be858eee3e1a18616483f6043ba68d427888a0be
jeff <jefferykibet@gmail.com>
2026-08-14 18:07:35 +0300

    Fix addHabit function and update context provider value in HabitProvider

--- src/context/HabitContext.jsx
+++ src/context/HabitContext.jsx
@@ -4,8 +4,12 @@ export const HabitContext = createContext();
 export function HabitProvider({ children }) {
   const [habits, setHabits] = useState([]);
 
+  function addHabit(habit) {
+    setHabits((currentHabits) => [...currentHabits, habit])
+  }
+
   return (
-    <HabitContext.Provider value={{ habits, setHbaits }}>
+    <HabitContext.Provider value={{ habits, addHabits}}>
       {children}
     </HabitContext.Provider>
   );
