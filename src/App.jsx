5d64290968df13b7dd19fbb2860e1150a531c2f8
Fiona <fionambuguanjeri@gmail.com>
2026-08-16 23:27:25 +0300

    Refactor Navbar and Footer components into the components directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
