3ab119697fb249e94128179446f88d1b54f1df9b
Fiona <fionambuguanjeri@gmail.com>
2026-08-16 13:21:13 +0300

    Add Home component with introductory content

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <main>
+      <section>
+        <h2>Build small habits. Grow every day. 🌸</h2>
+
+        <p>HabitBloom helps you build and track positive daily habits.</p>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
