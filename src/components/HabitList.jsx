12b60e2aa67c29f04625b64ee376a1e20d9d43d1
Boiwo Kipkurui Benard <boiwo.victor@student.moringaschool.com>
2026-08-17 13:35:06 +0300

    Complete habit management functionality

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,30 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HabitCard from "./HabitCard";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+function HabitList({ habits, onDelete, onToggle }) {
+  if (habits.length === 0) {
+    return <p>No habits found.</p>;
+  }
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import AddHabit from "./pages/AddHabit";
-import Habit from "./pages/Habit";
-import HabitDetails from "./pages/HabitDetails";
-
-function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/habits" element={<Habit />} />
-        <Route path="/habits/new" element={<AddHabit />} />
-        <Route path="/habits/:id" element={<HabitDetails />} />
-      </Routes>
-
-      <Footer />
-    </BrowserRouter>
+    <div className="habit-list">
+      {habits.map((habit) => (
+        <HabitCard
+          key={habit.id}
+          habit={habit}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
   );
 }
 
-export default App;
+export default HabitList;
