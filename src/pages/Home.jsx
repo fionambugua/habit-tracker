0c1041c40b184a8097a92a3775eda019f094b0e7
jeff <jefferykibet@gmail.com>
2026-08-18 10:19:03 +0300

    Add hero section styling and restructure Home component layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
+import heroImage from "../assets/tracker.jpg"
+
 function Home() {
   return (
     <main>
       <section className="hero">
-        <h2>Build small habits. Grow every day. </h2>
+        <div>
+          <h2>Build small habits. Grow every day.</h2>
 
-        <p>Habit Tracker helps you build and track positive daily habits.</p>
+          <p>
+            Habit Tracker helps you build and track positive daily habits.
+          </p>
+        </div>
+
+        <div>
+          <img src={heroImage} alt="Habit Tracker" />
+        </div>
       </section>
     </main>
   );
